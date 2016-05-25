ece46ff83b96a86474b32525a56ba20463e6d2e0
Joe Bidon <mrbidon@gmail.com>
2016-05-25 16:11:52 +0200

    Make the app works with standard CSV parse

--- js/geguide/GeGuide.js
+++ js/geguide/GeGuide.js
@@ -1,11 +1,28 @@
 var geguide = geguide ||  {};
 
 
+function filter_quote(fields){
+//	console.log(fields);
+	results = [];
+
+	for(var i = 0; i < fields.length; i++){
+		field = fields[i];
+		if((field[0] == '"') &&(field[field.length-1] == '"') ){
+			results.push(field.substr(1,field.length-1))
+		}else{
+			results.push(field)
+		}
+
+	}
+
+	return results
+}
+
 geguide.GeGuide = function(html_id){
 	this.mercator = new OpenLayers.Projection("EPSG:900913");
 	this.gps = new OpenLayers.Projection("EPSG:4326");
-	
-	
+
+
 	this.$main = $("#"+html_id);
 	this.$map = $("<div id='map'></div>").appendTo(this.$main);
 
@@ -19,19 +36,19 @@ geguide.GeGuide = function(html_id){
 	this.map.addLayers([new OpenLayers.Layer.OSM()]);
 
     this.map.setCenter(
-        new OpenLayers.LonLat(10.2, 48.9).transform(
+        new OpenLayers.LonLat(3.999, 49.596).transform(
             new OpenLayers.Projection("EPSG:4326"),
             this.map.getProjectionObject()
-        ), 
+        ),
         5
     );
-	
+
 	var vector = new OpenLayers.Layer.Vector("Points Of G.E. Interest", {
 	        styleMap: new OpenLayers.StyleMap({
 	        	"default":{
 		        	graphicWidth: 21,
 		            graphicHeight: 25,
-		            graphicYOffset: -28, 
+		            graphicYOffset: -28,
 		            externalGraphic: "${icon}"
 				},
 				"select":{
@@ -39,71 +56,69 @@ geguide.GeGuide = function(html_id){
 				}}
 	        )
 	    });
-	    
-	    
+
+
 	this.map.addLayers([vector]);
-	 
+
 	$.ajax({
 		url: "data/geguide.csv?"+Math.random(),
 		dataType: "text",
 		context: this,
 		contentType: "text/plain; charset=utf-8",
 		success: function(data){
-			var lines = null;
-			//gestion saut de ligne windows ou linux
-			if(data.search("\r\n") != -1){
-				 lines = data.split("\r\n");
-			}else{
-				 lines = data.split("\n");
-			}
-			
+
+			gastro_registry = geguide.CSVToArray(data);
+
+
 			// create the layer styleMap that uses the above style for all render intents
 			var adresses = [];
 		    var features = [];
-			for(var i = 1; i < lines.length; i++){
-				if(lines[i] != ""){
-					var line = lines[i].split(";");
-					var adresse = 	{
-									 "id":  parseInt(line[0]),	
-									 "titre": line[1],
-									 "detail": line[2], 
-									 "adresse": line[3],
-									 "latitude":   parseFloat(line[4]),
-									 "longitude": parseFloat(line[5]),
-									 "tags" : line[6].split(","),
-									 "dernierevisite": line[7],
-									 "geredacteur": line[8]		
-									};
-					
-					
-					//personalisation de l'icone en fonction du tag
-					adresse.icon = "js/lib/OpenLayers-2.13.1/img/marker-blue.png";
-					adresse.icon_hover = "js/lib/OpenLayers-2.13.1/img/marker-green.png";
-					if(adresse.tags.length>0){
-						if(adresse.tags[0] == "MiamMiam"){
-							adresse.icon = "img/chaudron-nofire.png";
-							adresse.icon_hover = "img/chaudron.png";
-						}else if(adresse.tags[0] == "GlouGlou"){
-							adresse.icon = "img/chope_full.png";
-							adresse.icon_hover = "img/chope_empty.png";
-						}					
-					}
-					adresses.push(adresse);
-					 
-				    features.push(new OpenLayers.Feature.Vector(
-						            new OpenLayers.Geometry.Point(
-							                parseFloat(adresse.longitude),
-							                parseFloat(adresse.latitude)
-						               	).transform(this.gps,this.mercator),
-						            adresse)
-					         );	
-				}
 
+			console.log(gastro_registry);
+			for(var i = 1; i < gastro_registry.length; i++){
+				line = gastro_registry[i];
+				var adresse = 	{
+												 "id":  parseInt(line[0]),
+												 "titre": line[1],
+												 "detail": line[2],
+												 "adresse": line[3],
+												 "latitude":   parseFloat(line[4]),
+												 "longitude": parseFloat(line[5]),
+												 "tags" : line[6].split(","),
+												 "dernierevisite": line[7],
+												 "geredacteur": line[8]
+												};
+
+
+				//personalisation de l'icone en fonction du tag
+				adresse.icon = "js/lib/OpenLayers-2.13.1/img/marker-blue.png";
+				adresse.icon_hover = "js/lib/OpenLayers-2.13.1/img/marker-green.png";
+				if(adresse.tags.length>0){
+					if(adresse.tags[0] == "MiamMiam"){
+						adresse.icon = "img/chaudron-nofire.png";
+						adresse.icon_hover = "img/chaudron.png";
+					}else if(adresse.tags[0] == "GlouGlou"){
+						adresse.icon = "img/chope_full.png";
+						adresse.icon_hover = "img/chope_empty.png";
+					}
+				}
+				adresses.push(adresse);
+
+		    features.push(new OpenLayers.Feature.Vector(
+				            new OpenLayers.Geometry.Point(
+					                parseFloat(adresse.longitude),
+					                parseFloat(adresse.latitude)
+				               	).transform(this.gps,this.mercator),
+				            adresse)
+			         );
 			}
+
+
 			vector.addFeatures(features);
+			console.log(features)
 			this.map.zoomToExtent(vector.getDataExtent());
 			vector.redraw();
-			
+
 			//tableau avec la liste des adresses
 			this.$displayListAdress.load(adresses);
 		}
@@ -116,7 +131,7 @@ geguide.GeGuide = function(html_id){
 	    // 'this' is the popup.
 	    selectControl.unselect(this.feature);
 	}
-	
+
 	function onFeatureSelect(evt) {
 	    //feature = evt.feature;
 	    popup = new OpenLayers.Popup.FramedCloud("featurePopup",
@@ -138,7 +153,7 @@ geguide.GeGuide = function(html_id){
 	        evt.popup = null;
 	    }
 	}
-	
+
 	 var selectControlHover = new OpenLayers.Control.SelectFeature(vector, {
           hover: true,
           highlightOnly: true});
@@ -147,14 +162,13 @@ geguide.GeGuide = function(html_id){
 
 
 	var selectControl = new OpenLayers.Control.SelectFeature(vector,
-            						{ 	
-            							toggle: true, 
-            							onSelect: onFeatureSelect, 
+            						{
+            							toggle: true,
+            							onSelect: onFeatureSelect,
             							onUnselect: onFeatureUnselect
             						});
 	this.map.addControl(selectControl);
 	selectControl.activate();
-	
-	
-}
 
+
+}
